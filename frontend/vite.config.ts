94a61d7f892600a55bde1b5381f57fe25282cb45
thePeras <jtopereira@gmail.com>
2025-05-18 19:35:37 +0100

    Setup env files

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,28 +1,32 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server: {
-    watch: {
-      usePolling: true,
-    },
-    hmr: {
-      overlay: true
-    },
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false
+    server: {
+      watch: {
+        usePolling: true,
+      },
+      hmr: {
+        overlay: true
+      },
+      proxy: {
+        '/api': {
+          target: `http://localhost:${env.VITE_API_PORT || 5000}`,
+          changeOrigin: true,
+          secure: false //TODO: Change later, for deliver / production, to enable SSL
+        }
       }
     }
   }
